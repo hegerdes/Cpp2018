2b39aad8b506eabdf0c2972f0c47927a24cf39f1
hegerdes <hegerdes@uni-osnabrueck.de>
2019-01-14 01:03:28 +0100

    Fixed movement for Bulled

--- uebung10/rendering/Bullet.cpp
+++ uebung10/rendering/Bullet.cpp
@@ -32,7 +32,12 @@ void Bullet::run()
     int i = 0;
     // Modify the bullet's position until the lifetime is over
 	while(i < Bullet::m_lifetime){
-		m_sphere.move(Renderable3D::ACCEL, 5);
+
+        // Vector3f pos = m_sphere.getPosition() + m_flightAxis;
+        // pos.invert();
+        //m_sphere.move(Renderable3D::ACCEL, 5);
+        m_sphere.setPosition(m_sphere.getPosition() + m_flightAxis);
+		
 		i++;
 		std::this_thread::sleep_for(std::chrono::microseconds(1000));
 	}
