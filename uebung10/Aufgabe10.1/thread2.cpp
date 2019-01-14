dda05011b047abdfb6fb8218e061bca626d012f6
hegerdes <hegerdes@uni-osnabrueck.de>
2019-01-14 01:42:49 +0100

    Fixed type of square_sum

--- uebung10/Aufgabe10.1/thread2.cpp
+++ uebung10/Aufgabe10.1/thread2.cpp
@@ -6,30 +6,32 @@
 
 using namespace std;
 
-//int square_sum = 0;
+atomic<int> square_sum(0);
 
-void pow2(int& square_sum, int x)
+int pow2(int x)
 {
-    int s = square_sum.load(memory_order_relaxed);
-    square_sum.store(s + x * x, memory_order_relaxed)
-}
+    int s;
+    s = square_sum.load(memory_order_relaxed);
+    square_sum.store(s + x * x, memory_order_relaxed);
+
+    return square_sum.load(memory_order_relaxed);
+};
 
 int main(int argc, char const *argv[])
 {
-    //auto square_sum = std::ref(int zero(0));
     std::atomic<int> square_sum(0);
 
     vector<thread> threads;
     for (int i = 1; i <= 20; i++)
     {
-        threads.push_back(thread(&pow2, square_sum, i));
+        threads.push_back(thread(pow2, i));
     }
 
     vector<thread>::iterator it;
     for (it = threads.begin(); it != threads.end(); it++)
     {
-       (*it).join();
+       it->join();
     }
-    cout << "Sum auf squares up to 20 is = " << square_sum << endl;
+    cout << "Sum auf squares up to 20 is = " << pow2(0) << endl;
     return 0;
 }
