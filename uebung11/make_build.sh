f23aea5f11ea72c54ecb8ba5f8f70cb0d736c870
hegerdes <hegerdes@uni-osnabrueck.de>
2019-01-16 21:25:46 +0100

    Added buildscript

---
+++ uebung11/make_build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+dir=build
+
+if [ $# -eq 0 ]; then
+    if [ ! -d "$dir" ]; then
+        mkdir build
+        cd build
+        cmake ..
+        make -j
+    else
+        cd "$dir"
+        make -j
+    fi
+else
+    if [ "$1" = "debug" ]; then
+        if [ ! -d "$dir" ]; then
+            mkdir build
+        fi
+        cd "$dir"
+        cmake -DCMAKE_BUILD_TYPE=Debug ..
+        echo makefile for Debug done
+        make -j
+    fi
+    if [ "$1" = "clean" ]; then
+        if [ ! -d "$dir" ]; then
+            echo Nothing to clean
+        fi
+        rm "$dir" -r
+    fi
+    if [ "$1" = "lunch" ]; then
+        if [ ! -d "$dir" ]; then
+        mkdir build
+        cd build
+        cmake ..
+        make -j
+    else
+        cd "$dir"
+        make -j
+    fi
+        ./asteroids ../models/
+    fi
+fi
+
+
