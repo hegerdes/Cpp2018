600f882a1309d2d1de46346bb46be4bb9683882a
hegerdes <hegerdes@uni-osnabrueck.de>
2019-01-27 23:32:50 +0100

    Added Foo and Traits

---
+++ uebung12/Aufgabe1/Foo.cpp
@@ -0,0 +1,27 @@
+#include <cstdint>
+
+template <bool Param, typename TrueResult, typename FalseResult>
+class foo;
+  
+template <typename TrueResult, typename FalseResult>
+struct foo<true, TrueResult, FalseResult>
+{
+    typedef TrueResult result;
+};
+ 
+template <typename TrueResult, typename FalseResult>
+struct foo<false, TrueResult, FalseResult>
+{
+    typedef FalseResult result;
+};
+
+
+int main()
+{
+   typename foo<true, int, void*>::result number(3);
+   typename foo<false, int, void*>::result pointer(&number);
+
+   typedef typename foo<(sizeof(void *) > sizeof(uint32_t)), uint64_t, uint32_t>::result integral_ptr_t;
+
+   integral_ptr_t converted_pointer = reinterpret_cast<integral_ptr_t>(pointer);
+}
